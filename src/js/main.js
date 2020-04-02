8b2d447c8401ad3993d62c4d1f6e2b92952d46ac
RiaRiver <60794418+RiaRiver@users.noreply.github.com>
2020-04-02 04:13:05 +0300

    feat: added header & promo, styles, PP & adaptive, checked FF FFm CH CHm OP IE EDGE

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+// Promo Slider
+var promoSwiper = new Swiper('.promo-swiper', {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  pagination: {
+    el: '.promo-pagination',
+    clickable: true
+  }
+})
